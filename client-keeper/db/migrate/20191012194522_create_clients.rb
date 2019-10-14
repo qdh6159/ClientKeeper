44f8d05c95dba2cfa79fb48695bf36e392ac3f3d
Quincy Hicks <qdh6159@gmail.com>
2019-10-14 10:32:26 -0500

    controller & schema

---
+++ client-keeper/db/migrate/20191012194522_create_clients.rb
@@ -0,0 +1,17 @@
+class CreateClients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :social
+      t.string :rating
+      t.string :last_contact
+      t.string :notes
+
+    end
+  end
+end
--- client-keeper/db/schema.rb
+++ client-keeper/db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_12_194522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "social"
+    t.string "rating"
+    t.string "last_contact"
+    t.string "notes"
+  end
 
 end
