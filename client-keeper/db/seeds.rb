cbd2aea25f182ff92a2bdf12b99b1412d2d483ec
Quincy Hicks <qdh6159@gmail.com>
2019-10-16 13:41:32 -0500

    seed & schema test

--- client-keeper/db/seeds.rb
+++ client-keeper/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+    Client.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      street: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state, 
+      zip: Faker::Address.zip_code,
+      social: Faker::Number.number(digits: 4),
+      rating: Faker::Number.between(from: 1, to: 4),
+      last_contact: Faker::Date.between(from: 2.years.ago, to: Date.today),
+      notes: Faker::Lorem.paragraph(sentence_count: 4)
+    )
+  end
