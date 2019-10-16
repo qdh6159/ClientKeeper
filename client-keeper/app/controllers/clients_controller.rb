86815bfc7ede55ddb623992e6b6c2a101e03493c
Quincy Hicks <qdh6159@gmail.com>
2019-10-16 14:07:19 -0500

    updated Index and Show routes

--- client-keeper/app/controllers/clients_controller.rb
+++ client-keeper/app/controllers/clients_controller.rb
@@ -1,2 +1,13 @@
 class ClientsController < ApplicationController
+
+    def index
+        render json: Client.all
+        # render json: {status: 200, clients: clients}
+    end
+
+    def show
+        puts params
+        render json: Client.find(params[:id])
+        # render json: {status: 200, client: client}
+      end
 end
